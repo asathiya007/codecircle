54f10e0f4e8a34325c4ddc4aa0ace9c760ae11b4
asathiya007 <akshay.sathiya@gmail.com>
2019-12-21 13:23:25 -0800

    set up basic express server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express(); 
+app.use(express.json());
+app.use(cors());
+
+// @route   GET /
+// @desc    test API running 
+// @access  public 
+app.get("/", (req, res) => res.json({msg: 'API running'}));
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
