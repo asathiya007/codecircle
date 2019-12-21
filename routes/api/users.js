48b6b52156c58dae92403b37acec12e26f71089c
asathiya007 <akshay.sathiya@gmail.com>
2019-12-21 14:32:55 -0800

    backend route for register working

---
+++ routes/api/users.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const {check, validationResult} = require('express-validator');
+const router = express.Router(); 
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+// @route   GET /api/users/ 
+// @desc    test users route -- TESTING ONLY 
+// @access  public 
+// router.get('/', (req, res) => res.json({msg: 'testing users route'}));
+
+// @route   POST /api/users/
+// @desc    register users
+// @access  public 
+router.post('/', 
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email address').isEmail(), 
+        check('password', 'Please provide a password of at least seven characters').isLength({min: 7})
+    ],
+    async (req, res) => {
+        // verify and extract input
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        let {name, email, password} = req.body; 
+
+        try {
+            // check if user already exists 
+            let user = await User.findOne({email});
+            if (user) {
+                return res.status(400).json({errors: [
+                    {msg: 'user with provided email already exists'}
+                ]}); 
+            }
+
+            // get new user gravatar 
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            }); 
+
+            // encrypt password 
+            const salt = await bcrypt.genSalt(10);
+            password = await bcrypt.hash(password, salt);
+
+            // create and save new user document
+            user = new User({
+                name, 
+                email, 
+                password, 
+                avatar
+            }); 
+            await user.save(); 
+
+            // create JWT and send to client 
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }; 
+            jwt.sign(
+                payload, 
+                config.get('jwtKey'), 
+                {expiresIn: 3600},
+                (err, token) => {
+                    if (err) throw err; 
+                    res.json({token});
+                }
+            ); 
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({errors: [
+                {msg: 'server error - unable to register user'}
+            ]}); 
+        }
+    }
+); 
+
+module.exports = router; 
--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const cors = require('cors');
+const connectDB = require("./config/db");
 
 const app = express(); 
 app.use(express.json());
 app.use(cors());
+connectDB(); 
 
 // @route   GET /
-// @desc    test API running 
+// @desc    test API running -- TESTING ONLY 
 // @access  public 
-app.get('/', (req, res) => res.json({msg: 'API running'}));
+// app.get('/', (req, res) => res.json({msg: 'API running'}));
+
+// other routes
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
