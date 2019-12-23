e315383f965e5d55d0a70bd6e1024bf4e87cf23b
asathiya007 <akshay.sathiya@gmail.com>
2019-12-23 13:06:50 -0800

    created routes for getting profile and creating/updating profile

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -41,7 +41,7 @@ export const register = ({name, email, password}) => async dispatch => {
             }
         };
         const body = JSON.stringify({name, email, password});
-        const res = await axios.post('api/users', body, config); 
+        const res = await axios.post('/api/users', body, config); 
 
         // send token to store 
         dispatch({
@@ -74,7 +74,7 @@ export const login = ({ email, password }) => async dispatch => {
             }
         };
         const body = JSON.stringify({ email, password });
-        const res = await axios.post('api/auth', body, config);
+        const res = await axios.post('/api/auth', body, config);
 
         // send token to store 
         dispatch({
---
+++ client/src/actions/profile.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import {
+    GET_PROFILE,
+    PROFILE_ERROR, 
+    CLEAR_PROFILE
+} from './types';
+import {setAlert} from './alert';
+
+export const getProfile = () => async dispatch => {
+    try {
+        // get current use profile data and send to store (profile)
+        const res = await axios.get('/api/profile/me');
+        dispatch({
+            type: GET_PROFILE, 
+            payload: res.data
+        }); 
+    } catch (error) {
+        dispatch({type: CLEAR_PROFILE});
+        dispatch({
+            type: PROFILE_ERROR, 
+            payload: {msg: 'Error in obtaining current user profile data'}
+        }); 
+    }
+}
+
+// create or update current user profile 
+export const buildProfile = (formData, history, edit = false) => async dispatch => {
+    try {
+        // create/update current user profile with new data 
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        const res = await axios.post('/api/profile', formData, config);
+
+        // send new profile data to store (profile)
+        dispatch({
+            type: GET_PROFILE, 
+            payload: res.data
+        }); 
+
+        // create alert to notify user of profile update
+        dispatch(setAlert(edit ? 'Profile updated' : 'Profile created', 'success'));
+
+        // redirect to dashboard 
+        history.push('/dashboard');
+    } catch (error) {
+        // create an alert for each error  
+        const errors = error.response.data.errors; 
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger'))); 
+        }
+
+        // clear user profile data (profile)
+        dispatch({
+            type: PROFILE_ERROR, 
+            payload: {msg: 'Error in creating/updating user profile data'}
+        }); 
+    }
+}
