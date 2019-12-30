ca7dbbd810ecfc0a2f31697c095b325b1aa68b1d
asathiya007 <akshay.sathiya@gmail.com>
2019-12-30 00:31:13 -0800

    created file upload backend route

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
 const express = require('express');
 const cors = require('cors');
-const connectDB = require("./config/db");
+const connectDB = require('./config/db');
+const tokenauth = require('./middleware/tokenauth');
+const File = require('./models/File');
+const fileUpload = require('express-fileupload');
 
 const app = express(); 
 app.use(express.json());
 app.use(cors());
+app.use(fileUpload());
 connectDB(); 
 
 // @route   GET /
 // @desc    test API running -- TESTING ONLY 
 // @access  public 
 // app.get('/', (req, res) => res.json({msg: 'API running'}));
+
+// @route   POST /uploads
+// @desc    file upload 
+// @access  private
+app.post('/uploads', tokenauth, async(req, res) => {
+    // create a new File object and send its id to the client
+    const file = new File({
+        user: req.user.id, 
+        ...req.files.file
+    }); 
+    await file.save(); 
+    res.json(file.id);
+}); 
 
 // other routes
 app.use('/api/users', require('./routes/api/users'));
