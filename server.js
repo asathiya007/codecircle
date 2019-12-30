5147dadd9f944b13b8d399a6b934b1bdddd76950
asathiya007 <akshay.sathiya@gmail.com>
2019-12-30 00:34:01 -0800

    preparing to work on the posts api routes

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.post('/uploads', tokenauth, async(req, res) => {
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth')); 
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
