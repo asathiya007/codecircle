1639d4031ba95eaff8cd4f48082b91d8c8a2ddb6
asathiya007 <akshay.sathiya@gmail.com>
2020-01-01 23:28:00 -0800

    preparing for deployment to Heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const tokenauth = require('./middleware/tokenauth');
 const File = require('./models/File');
 const fileUpload = require('express-fileupload');
+const path = require('path');
 
 const app = express(); 
 app.use(express.json());
@@ -34,6 +35,17 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth')); 
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// serve static assets in production 
+if (process.env.NODE_ENV === 'production') {
+    // set static folder 
+    app.use(express.static('client/build'));
+
+    // use server-side rendering to render the client 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')); 
+    }); 
+}
 
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
