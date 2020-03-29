890d11eb28400b4aaf110fdac15cac2d1fd88c91
asathiya007 <akshay.sathiya@gmail.com>
2020-03-29 19:48:27 -0400

    checked all React Moment instances for inclusion or exclusion of utc attribute, fixed up GitHub repo display and fetching endpoint

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -314,7 +314,7 @@ router.get('/github/:username', (req, res) => {
     try {
         // fetch GitHub repos from GitHub API by GitHub username
         const options = {
-            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}`,
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=updated&client_id=${config.get("githubClientId")}&client_secret=${config.get("githubSecret")}`,
             method: 'GET',
             headers: {'user-agent': 'node.js'}
         }
