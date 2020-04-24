4f4611cc40e9e159ecad46f3bb7bb22b84db50a1
asathiya007 <akshay.sathiya@gmail.com>
2020-04-24 17:56:29 -0400

    stores and deletes connections in MongoDB, can broadcast messages

--- client/src/components/chats/Chats/Chats.js
+++ client/src/components/chats/Chats/Chats.js
@@ -1,20 +1,23 @@
 import React, {useEffect} from 'react'; 
 import socketIOClient from "socket.io-client";
-// import PropTypes from 'prop-types'; 
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types'; 
 
-const Chats = props => {
+const Chats = ({user}) => {
     useEffect(() => {
-        try {
-          const socket = socketIOClient(process.env.PORT);
-          socket.on("chat message", (data) => {
-            console.log(data);
-          });
-        } catch (error) {
-          console.log("Connecting to socket...");
+        if (user) {
+            try {
+                const socket = socketIOClient(process.env.PORT, { query: `user=${user._id}` });
+                socket.on("chat message", (data) => {
+                    console.log(data);
+                });
+            } catch (error) {
+                console.log("Connecting to socket...");
+            }
         }
-    }, []); 
+    }, [user]); 
 
-    return (
+    return user && (
         <div className="h-center top-space bottom-space">
             <div className="w-60">
                 <p className="f1 fw7 text-primary mv0">
@@ -29,8 +32,12 @@ const Chats = props => {
     )
 }
 
-// Chats.propTypes = {
+Chats.propTypes = {
+    userId: PropTypes.string.isRequired
+}
 
-// }
+const mapStateToProps = state => ({
+    user: state.auth.user
+}); 
 
-export default Chats; 
+export default connect(mapStateToProps)(Chats); 
